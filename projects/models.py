ff5607c6446467d816827d8e9f55e3294d1f1686
ash-eff <ash.s.fischer@gmail.com>
2023-11-03 12:25:13 -0500

    Add UserProject model

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import CustomUser
 
 
 class Project(models.Model):
@@ -7,3 +8,15 @@ class Project(models.Model):
 
     def __str__(self):
         return self.project_name
+
+
+class UserProject(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ['user', 'project']
+
+    def __str__(self):
+        return f'{self.user.username} - {self.project.project_name}'
